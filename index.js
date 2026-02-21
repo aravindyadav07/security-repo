4bf33bb67f32b1e6fa38abed7a3a08dcc826ed5d
aravind <aravindyadav991343@gmail.com>
2026-02-21 09:54:06 +0530

    Introduce failing security checks

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const lodash = require('lodash');
 
+// Hardcoded secret for testing gitleaks
+const AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE";
 // Read API key from environment for security
 const API_KEY = process.env.API_KEY || "Not provided";
 
